ff21902713404fd162b30f98227490a47b0c10f8
Justin <jusmar3799@stu.mylusd.org>
2026-06-05 15:06:45 -0700

    Contains Python Code

---
+++ Code/mariocode.py
@@ -0,0 +1,66 @@
+from gpiozero import Button, LED
+from signal import pause
+
+mushroom_button = Button(17)
+fireflower_button = Button(27)
+enemyhit_button = Button(22)
+
+small_led = LED(18)
+super_led = LED(23)
+fire_led = LED(24)
+
+SMALL = "Small Mario"
+SUPER = "Super Mario"
+FIRE = "Fire Mario"
+
+state = SMALL
+
+def update_outputs():
+    if state == SMALL:
+        small_led.on()
+        super_led.off()
+        fire_led.off()
+
+    elif state == SUPER:
+        small_led.off()
+        super_led.on()
+        fire_led.off()
+
+    elif state == FIRE:
+        small_led.off()
+        super_led.off()
+        fire_led.on()
+
+def mushroom_pressed():
+    global state
+
+    if state == SMALL:
+        state = SUPER
+
+    update_outputs()
+
+def fireflower_pressed():
+    global state
+
+    if state == SMALL or state == SUPER:
+        state = FIRE
+
+    update_outputs()
+
+def enemyhit_pressed():
+    global state
+
+    if state == FIRE:
+        state = SUPER
+    elif state == SUPER:
+        state = SMALL
+
+    update_outputs()
+
+mushroom_button.when_pressed = mushroom_pressed
+fireflower_button.when_pressed = fireflower_pressed
+enemyhit_button.when_pressed = enemyhit_pressed
+
+update_outputs()
+
+pause()
